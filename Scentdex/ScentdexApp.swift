e542fb58c35ae8db84a766e5debdc20f1216ba81
rcavalcante11 <dicavalcante18@gmail.com>
2026-03-19 11:44:10 +0000

    Initial Commit

---
+++ Scentdex/ScentdexApp.swift
@@ -0,0 +1,32 @@
+//
+//  ScentdexApp.swift
+//  Scentdex
+//
+//  Created by macbook on 19/03/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ScentdexApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
