3c5aa99931f1a3b3dd5f1710943c3b835ba8c44a
rcavalcante11 <dicavalcante18@gmail.com>
2026-03-20 18:53:32 +0000

    Add FragranceFamily enum with CaseIterable and Codable

---
+++ Models/FragranceFamily.swift
@@ -0,0 +1,20 @@
+//
+//  FragranceFamily.swift
+//  Scentdex
+//
+//  Created by macbook on 20/03/2026.
+//
+
+import Foundation
+
+enum FragranceFamily: String, CaseIterable, Codable {
+    case floral = "Floral"
+    case woody = "Woody"
+    case oriental = "Oriental"
+    case fresh = "Fresh"
+    case citrus = "Citrus"
+    case aquatic = "Aquatic"
+    case gourmand = "Gourmand"
+    case spicy = "Spicy"
+    case herbal = "Herbal"
+}
